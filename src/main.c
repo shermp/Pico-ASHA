6b6a4779eb9f922f82f737aa00d07c6d4f0935e7
Sherman Perry <14854761+shermp@users.noreply.github.com>
2024-06-19 21:18:11 +1200

    Fix saving pairing information
    Apparently, multi-core throws a spanner in the works.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include <pico/stdio.h>
 #include <pico/multicore.h>
+#include <pico/flash.h>
 
 #include "asha_audio.h"
 
@@ -17,6 +19,14 @@ int main()
     asha_audio_init(&asha_shared);
     stdio_init_all();
     sleep_ms(2000);
+    // Apparently there be dragons when using flash and
+    // multicore. And guess where BTStack saves pairing
+    // info? Calling this function on the current core
+    // (core 0) should make it work.
+    if (!flash_safe_execute_core_init()) {
+        printf("flash_safe_execute_core_init failed."
+               "Pairing data will not be saved.\n");
+    }
 #ifdef ASHA_PERF_TEST
     multicore_launch_core1(perf_main);
 #else
