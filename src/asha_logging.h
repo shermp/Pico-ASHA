b4f57c38bacfe32c3d51b73663e650e7eff622b7
Sherman Perry <14854761+shermp@users.noreply.github.com>
2024-07-21 13:14:26 +1200

    Add simple timestamp to logging
    The timestamp is in ms from boot

--- src/asha_logging.h
+++ src/asha_logging.h
@@ -2,6 +2,7 @@
 
 #if defined (ASHA_LOG_ERROR) || defined (ASHA_LOG_INFO) || defined (ASHA_LOG_AUDIO)
 #include <stdio.h>
+#include "pico/time.h"
 #endif
 
 #ifdef __cplusplus
@@ -9,29 +10,29 @@ extern "C" {
 #endif
 
 #if defined (ASHA_LOG_ERROR)
-    #define LOG_ERROR(...) printf("[Pico-ASHA ERROR] " __VA_ARGS__)
-    #define LOG_INFO(...)
-    #define LOG_SCAN(...)
-    #define LOG_AUDIO(...)
+    #define LOG_ERROR(fmt, ...) printf("[%llu ms] [ASHA ERROR] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_INFO(fmt, ...)
+    #define LOG_SCAN(fmt, ...)
+    #define LOG_AUDIO(fmt, ...)
 #elif defined (ASHA_LOG_INFO)
-    #define LOG_ERROR(...) printf("[Pico-ASHA ERROR] " __VA_ARGS__)
-    #define LOG_INFO(...) printf("[Pico-ASHA  INFO] " __VA_ARGS__)
-    #define LOG_SCAN(...)
-    #define LOG_AUDIO(...)
+    #define LOG_ERROR(fmt, ...) printf("[%u ms] [ASHA ERROR] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_INFO(fmt, ...) printf("[%u ms] [Pico-ASHA  INFO] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_SCAN(fmt, ...)
+    #define LOG_AUDIO(fmt, ...)
 #elif defined (ASHA_LOG_SCAN)
-    #define LOG_ERROR(...) printf("[Pico-ASHA ERROR] " __VA_ARGS__)
-    #define LOG_INFO(...) printf("[Pico-ASHA  INFO] " __VA_ARGS__)
-    #define LOG_SCAN(...) printf("[Pico-ASHA  SCAN] " __VA_ARGS__)
-    #define LOG_AUDIO(...) 
+    #define LOG_ERROR(fmt, ...) printf("[%u ms] [ASHA ERROR] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_INFO(fmt, ...) printf("[%u ms] [ASHA  INFO] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_SCAN(fmt, ...) printf("[%u ms] [ASHA  SCAN] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_AUDIO(fmt, ...) 
 #elif defined (ASHA_LOG_AUDIO)
-    #define LOG_ERROR(...) printf("[Pico-ASHA ERROR] " __VA_ARGS__)
-    #define LOG_INFO(...) printf("[Pico-ASHA  INFO] " __VA_ARGS__)
-    #define LOG_SCAN(...) printf("[Pico-ASHA  SCAN] " __VA_ARGS__)
-    #define LOG_AUDIO(...) printf("[Pico-ASHA AUDIO] " __VA_ARGS__)
+    #define LOG_ERROR(fmt, ...) printf("[%u ms] [ASHA ERROR] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_INFO(fmt, ...) printf(" [%u ms] [ASHA  INFO] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_SCAN(fmt, ...) printf("[%u ms] [ASHA  SCAN] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
+    #define LOG_AUDIO(fmt, ...) printf("[%u ms] [ASHA AUDIO] " fmt, to_ms_since_boot(get_absolute_time()), ##__VA_ARGS__)
 #else
-    #define LOG_ERROR(...)
-    #define LOG_INFO(...)
-    #define LOG_AUDIO(...)
+    #define LOG_ERROR(fmt, ...)
+    #define LOG_INFO(fmt, ...)
+    #define LOG_AUDIO(fmt, ...)
 #endif
 
 #ifdef __cplusplus
