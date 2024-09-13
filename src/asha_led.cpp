434401cb30c19f83f3ff30f90985b07edea1de93
Sherman Perry <14854761+shermp@users.noreply.github.com>
2024-09-13 20:28:28 +1200

    Create LED manager class

---
+++ src/asha_led.cpp
@@ -0,0 +1,50 @@
+#include <bitset>
+
+#include "asha_led.hpp"
+#include "pico/cyw43_arch.h"
+
+namespace asha
+{
+
+LEDManager::LEDManager()
+{
+    led_worker.user_data = this;
+}
+
+void LEDManager::set_led(LEDManager::State led_state)
+{
+    disable_curr_led_pattern();
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, (led_state == State::On) ? true : false);
+}
+
+void LEDManager::set_led_pattern(LEDManager::Pattern const& pattern)
+{
+    if (pattern.len > 32 || !led_ctx) return;
+    disable_curr_led_pattern();
+    curr_pattern = pattern;
+    async_context_add_at_time_worker_in_ms(led_ctx, &led_worker, 0);
+}
+
+void LEDManager::handle_led_work(async_context_t* ctx, async_at_time_worker_t *worker)
+{
+    LEDManager *this_ = (LEDManager*)worker->user_data;
+    Pattern& p = this_->curr_pattern;
+    std::bitset<32> pattern_bitset(p.pattern);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, pattern_bitset[p.pos]);
+    ++p.pos;
+    if (p.pos >= p.len) {
+        p.pos = 0;
+        async_context_add_at_time_worker_in_ms(this_->led_ctx, &this_->led_worker, p.interval_ms + p.delay_ms);
+    } else {
+        async_context_add_at_time_worker_in_ms(this_->led_ctx, &this_->led_worker, p.interval_ms);
+    }
+}
+
+void LEDManager::disable_curr_led_pattern()
+{
+    if (led_ctx) {
+        async_context_remove_at_time_worker(led_ctx, &led_worker);
+    }
+}
+
+} // namespace asha
