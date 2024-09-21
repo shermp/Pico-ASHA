08257783978a2b02f5dc60205a5c1a0039a5300c
Sherman Perry <14854761+shermp@users.noreply.github.com>
2024-09-21 21:49:58 +1200

    Asha logging warning fixes

--- src/asha_logging.cpp
+++ src/asha_logging.cpp
@@ -5,7 +5,7 @@
 namespace asha
 {
 
-void handle_logging_pending_worker(async_context_t *context, async_when_pending_worker_t *worker)
+void handle_logging_pending_worker([[maybe_unused]] async_context_t *context, [[maybe_unused]] async_when_pending_worker_t *worker)
 {
     while(!log_buffer.empty() && stdio_usb_connected()) {
         printf("%s", log_buffer.front().c_str());
