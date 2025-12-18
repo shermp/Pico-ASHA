e2e6ec7eae20d04312642dd85cefb40a8b7c4795
shermp <14854761+shermp@users.noreply.github.com>
2025-12-18 17:15:25 +1300

    Removed log level and UART from runtime settings

--- src/runtime_settings.cpp
+++ src/runtime_settings.cpp
@@ -5,21 +5,6 @@
 namespace asha
 {
 
-enum LogLevel str_to_log_level(const char* log_level)
-{
-    if (str_eq(log_level, "ERROR")) {
-        return LogLevel::Error;
-    } else if (str_eq(log_level, "INFO")) {
-        return LogLevel::Info;
-    } else if (str_eq(log_level, "SCAN")) {
-        return LogLevel::Scan;
-    } else if (str_eq(log_level, "AUDIO")) {
-        return LogLevel::Audio;
-    } else {
-        return LogLevel::None;
-    }
-}
-
 void RuntimeSettings::init()
 {
     btstack_tlv_get_instance(&tlv_impl, &tlv_ctx);
@@ -31,27 +16,12 @@ void RuntimeSettings::init()
    to make updating default settings easier */
 void RuntimeSettings::get_settings()
 {
-    if (!get_tlv_tag(Tag::SerialUARTEnabled, serial_uart_enabled)) {
-        serial_uart_enabled = false;
-    }
     if (!get_tlv_tag(Tag::HCIDump, hci_dump_enabled)) {
         hci_dump_enabled = false;
     }
     if (!get_tlv_tag(Tag::FullSetPaired, full_set_paired)) {
         full_set_paired = false;
     }
-    if (!get_tlv_tag(Tag::LogLevel, log_level)) {
-        log_level = LogLevel::Info;
-    }
-}
-
-bool RuntimeSettings::set_uart_enabled(bool is_enabled)
-{
-    if (serial_uart_enabled != is_enabled) {
-        serial_uart_enabled = is_enabled;
-        return store_tlv_tag(Tag::SerialUARTEnabled, serial_uart_enabled);
-    }
-    return false;
 }
 
 bool RuntimeSettings::set_hci_dump_enabled(bool is_enabled)
@@ -68,15 +38,6 @@ bool RuntimeSettings::set_full_set_paired(bool have_full_set)
     if (full_set_paired != have_full_set) {
         full_set_paired = have_full_set;
         return store_tlv_tag(Tag::FullSetPaired, have_full_set);
-    }
-    return false;
-}
-
-bool RuntimeSettings::set_log_level(enum LogLevel level)
-{
-    if (log_level != level) {
-        log_level = level;
-        return store_tlv_tag(Tag::LogLevel, log_level);
     }
     return false;
 }
